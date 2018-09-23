915ca9dec7de972eb29cb5638d9d2e5a59c51fd3
eryshkov <wendor265>
2018-09-23 16:50:23 +0300

    fix equality

--- AR Image Finder/ViewController.swift
+++ AR Image Finder/ViewController.swift
@@ -41,10 +41,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         let referenceImages =
             ARReferenceImage.referenceImages(inGroupNamed: "AR Resources",
-                                            bundle: nil)!
+                                             bundle: nil)!
         
         configuration.detectionImages = referenceImages
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -55,13 +55,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
+    
     // MARK: - ARSCNViewDelegate
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let imageAnchor = anchor as? ARImageAnchor else { return }
         
-//        print(#function, "Найдена картинка \(imageAnchor.referenceImage.name ?? "")")
+        //        print(#function, "Найдена картинка \(imageAnchor.referenceImage.name ?? "")")
         
         switch anchor {
         case let imageAnchor as ARImageAnchor:
@@ -76,7 +76,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func nodeAdded(_ node: SCNNode, for imageAnchor: ARImageAnchor) {
         let referenceImage = imageAnchor.referenceImage
         
-        let plane = SCNPlane(width: referenceImage.physicalSize.width, height: referenceImage.physicalSize.height)
+        let plane = SCNPlane(width: referenceImage.physicalSize.width,
+                             height: referenceImage.physicalSize.height)
         
         plane.firstMaterial?.diffuse.contents = UIColor.blue
         
@@ -84,7 +85,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         planeNode.opacity = 0.25
         planeNode.eulerAngles.x = -Float.pi / 2
         
-        if isCoordsEqual(element: imageAnchor, accuracy: 0.05, array: anchorsArray) {
+        if isCoordsEqual(element: imageAnchor, accuracy: 0.001, array: anchorsArray) {
             sceneView.session.remove(anchor: imageAnchor)
         }else{
             moneyCount += 1
@@ -97,14 +98,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     func isCoordsEqual(element: ARAnchor, accuracy: Float, array: [ARAnchor]) -> Bool {
-        
         for item in array {
-            if (abs(element.transform.columns.3.x - item.transform.columns.3.x) <= accuracy) &&
-                (abs(element.transform.columns.3.y - item.transform.columns.3.y) <= accuracy) &&
-                (abs(element.transform.columns.3.z - item.transform.columns.3.z) <= accuracy)
-            {return true}
+//                        let x = element.transform.columns.3.x - item.transform.columns.3.x
+//                        let y = element.transform.columns.3.y - item.transform.columns.3.y
+//                        let z = element.transform.columns.3.z - item.transform.columns.3.z
+//                        let length = x*x + y*y + z*z
+//
+//                        if length <= accuracy*accuracy {return true}
+            if simd_almost_equal_elements_relative(element.transform, item.transform, accuracy) {return true}
+            
         }
-        
         return false
     }
     
